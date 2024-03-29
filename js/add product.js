8b443396db45b908fd92c39f6d209a67f3b60288
krunal5291 <krunalkalathiya11@gmail.com>
2024-03-29 11:57:52 +0530

    shorting

--- js/add product.js
+++ js/add product.js
@@ -8,15 +8,24 @@ const header = (e) => {
         titel: titel = document.getElementById("titel").value,
         img: img = document.getElementById("img").value,
         price: price = document.getElementById("price").value,
+        Category: category = document.getElementById("category").value
     }
     products.push(product);
     localStorage.setItem("product", JSON.stringify(products))
+
+
 }
 
-let islogin=localStorage.getItem("islogin");
-if(islogin==true)
-{
-    document.getElementById("login_1").innerHTML = "Logout";
-}
+
+
 
 document.getElementById("form").addEventListener("submit", header)
+// let islogin=localStorage.getItem("islogin");
+// if(islogin==true)
+// {
+//     document.getElementById("login_1").innerHTML = "Logout";
+// }
+// else
+// {
+//    window.location.href="/html/login.html";
+// }
--- js/product.js
+++ js/product.js
@@ -1,27 +1,82 @@
-let products = JSON.parse(localStorage.getItem("product")) || []
-const ui = () => {
-    document.getElementById("ui").innerHTML = ""
-    products.map((ele, i) => {
-        let h1 = document.createElement("h1")
-        h1.innerHTML = ele.titel
-        let img = document.createElement("img")
-        img.src = ele.img
-        let price = document.createElement("h3")
-        price.innerHTML = ele.price
+let products = JSON.parse(localStorage.getItem("product")) || [];
+const ui = (products) => {
+  document.getElementById("ui").innerHTML = "";
+  products.map((ele, i) => {
+    let h1 = document.createElement("h1");
+    h1.innerHTML = ele.titel;
+    let img = document.createElement("img");
+    img.src = ele.img;
+    let price = document.createElement("h3");
+    price.innerHTML = ele.price;
 
-        let dele = document.createElement("button")
-        dele.innerHTML = "delete"
+    let dele = document.createElement("button");
+    dele.innerHTML = "delete";
+    let Category = document.createElement("h6");
+    Category.innerHTML = ele.Category;
 
-        dele.addEventListener("click", () => {
-            products.splice(i, 1);
-            
-            ui()
-        })
+    let cart = document.createElement("button");
+    cart.innerHTML = "buy now";
 
-        let div = document.createElement("div")
-        div.append(img, h1, price, dele)
-        document.getElementById("ui").append(div)
+    cart.addEventListener("click", () => {
+
+      localStorage.setItem("cart",JSON.stringify(ele));
+
     })
+
+    dele.addEventListener("click", () => {
+      products.splice(i, 1);
+
+      ui(products);
+    });
+    
+
+
+    let div = document.createElement("div");
+    div.append(img, h1, price, dele, Category,cart);
+    document.getElementById("ui").append(div);
+  });
+
+};
+
+ui(products);
+
+
+
+const Sorting = (val) =>
+{
+    if(val=="htl")
+    {
+        products.sort((a,b) => b.price - a.price)
+    }
+    else
+    {
+        products.sort((a,b) => a.price - b.price)
+    }
+    ui(products)
+} 
+
+const Filter = (val) =>
+{
+   let temp =products.filter((ele) => ele.Category==val)
+   ui(temp)
 }
 
-ui()
+const Serach_Filter = (val) =>
+{
+    let add_temp =products.filter((ele) => ele.titel.includes(val))
+    ui(add_temp)
+}
+const serach = (e) =>
+{
+    e.preventDefault();
+    let Serach_Title = document.querySelector("#Search").value;
+    Serach_Filter(Serach_Title);
+}
+
+document.querySelector("#lth").addEventListener("click",() =>Sorting("low")); 
+document.querySelector("#htl").addEventListener("click",() =>Sorting("high")); 
+document.querySelector("#kid").addEventListener("click",() =>Filter("Kids"));
+document.querySelector("#male").addEventListener("click",() =>Filter("Male"));
+document.querySelector("#female").addEventListener("click",() =>Filter("Female"));
+document.querySelector("#serach").addEventListener("submit",serach)
+                                                                                                    
